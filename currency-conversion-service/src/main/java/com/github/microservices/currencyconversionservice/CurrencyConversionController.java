1b0ab4a01078840c4c4088a3ebce79a5923237d6
Jakub Wroński <wronski2002@wp.pl>
2024-03-10 15:50:42 +0100

    Added EUREKA server / load balancing

--- currency-conversion-service/src/main/java/com/github/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/github/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -11,6 +11,12 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+    private CurrencyExchangeProxy proxy;
+
+    public CurrencyConversionController(CurrencyExchangeProxy proxy) {
+        this.proxy = proxy;
+    }
+
     @GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
         HashMap<String, String> uriVariables = new HashMap<>();
@@ -27,6 +33,18 @@ public class CurrencyConversionController {
                 from, to, quantity,
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment());
+                currencyConversion.getEnvironment() + " " + "rest template");
+    }
+
+    @GetMapping("currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign");
     }
 }
